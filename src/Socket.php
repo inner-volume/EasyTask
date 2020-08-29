e4f66a39cfa79c8395b823c561e918480e1a3ee5
GaoJiuFeng <392223903@qq.com>
2020-08-29 11:54:21 +0800

    critical 有判断力的  adj

--- src/Socket.php
+++ src/Socket.php
@@ -12,62 +12,14 @@ use Exception;
 class Socket
 {
     /**
-     * 锁文件
+     * protocol
      * @var string
      */
-    private $file;
+    protected $protocol = 'tcp';
 
     /**
-     * 构造函数
-     * @param string $name
-     * @throws Exception
+     * host
+     * @var string
      */
-    public function __construct($name = 'lock')
-    {
-        //初始化文件
-        $path = FileHelper::getLokPath();
-        $this->file = $path . md5($name);
-        if (!file_exists($this->file))
-        {
-            @file_put_contents($this->file, '');
-        }
-    }
-
-    /**
-     * 加锁执行
-     * @param Closure $func
-     * @param bool $block
-     * @return mixed
-     */
-    public function execute($func, $block = true)
-    {
-        $fp = fopen($this->file, 'r');
-        $is_flock = $block ? flock($fp, LOCK_EX) : flock($fp, LOCK_EX | LOCK_NB);
-        $call_back = null;
-        if ($is_flock)
-        {
-            $call_back = $func();
-            flock($fp, LOCK_UN);
-        }
-        fclose($fp);
-        return $call_back;
-    }
-
-    /**
-     * 执行状态
-     */
-    public function executeStatus()
-    {
-        $file = $this->file;
-        if (!file_exists($file))
-        {
-            return false;
-        }
-        $fp = fopen($file, "r");
-        if (flock($fp, LOCK_EX | LOCK_NB))
-        {
-            return false;
-        }
-        return true;
-    }
+    protected $host = '127.0.0.1';
 }
